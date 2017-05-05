ef14b2d33f2c4bbd04f61abc5728bd6825b0790c
Mateus Bellozupko <mateus.bellozupko@madetoengage.com>
2017-05-05 11:26:51 -0300

    changing color

--- src/ios/CDVInAppBrowser.m
+++ src/ios/CDVInAppBrowser.m
@@ -599,6 +599,8 @@
     self.toolbar.multipleTouchEnabled = NO;
     self.toolbar.opaque = NO;
     self.toolbar.userInteractionEnabled = YES;
+    self.closeButton.tintColor = [UIColor whiteColor];
+    self.toolbar.barTintColor=[UIColor colorWithRed:123.0 / 255.0 green:194.0 / 255.0 blue:11.0 / 255.0 alpha:1];
 
     CGFloat labelInset = 5.0;
     float locationBarY = toolbarIsAtBottom ? self.view.bounds.size.height - FOOTER_HEIGHT : self.view.bounds.size.height - LOCATIONBAR_HEIGHT;
